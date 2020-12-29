731a8461a28702a3ab398405596d095a75dfe4be
Misel Evtimov <misel.evtimov@gmail.com>
2020-12-29 04:15:33 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -2,39 +2,39 @@
 def third_challenge
   epic_tragedy = {
    :montague => {
-      :patriarch => {
+      :patriarch => {[
         :name = {"Lord Montague"},
-        :age = {"53"}
+        :age = {"53"}]
         #your key/value pairs here
         },
-      :matriarch => {
+      :matriarch => {[
         :name = {"Lady Montague"},
-        :age = {"54"}
+        :age = {"54"}]
         #your key/value pairs here
         },
-      :hero => {
+      :hero => {[
         :name = {"Romeo"},
         :age = {"15"},
-        :status = {"alive"}
+        :status = {"alive"}]
         #your key/value pairs here
         },
       :hero_friends => []
    },
    :capulet => {
-      :patriarch => {
+      :patriarch => {[
         :name = {"Lord Capulet"},
-        :age = {"50"}
+        :age = {"50"}]
         #your key/value pairs here
         },
-      :matriarch => {
+      :matriarch => {[
         :name = {"Lady Capulet"},
-        :age = {"51"}
+        :age = {"51"}]
         #your key/value pairs here
         },
-      :heroine => {
+      :heroine => {[
         :name = {"Juliet"},
         :age = {"15"},
-        :status = {"alive"}
+        :status = {"alive"}]
         #your key/value pairs here
         },
       :heroine_friends => []
