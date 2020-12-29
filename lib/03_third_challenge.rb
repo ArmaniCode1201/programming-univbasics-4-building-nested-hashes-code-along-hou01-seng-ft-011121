3316884ed810a72b7443d80a2f4fd03371e277c6
Misel Evtimov <misel.evtimov@gmail.com>
2020-12-29 02:00:41 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -5,7 +5,7 @@ def third_challenge
         name: {'Lord Montague'},
         age: {'53'}
         #your key/value pairs here
-        }},
+        },
       :matriarch => {
         name: {'Lady Montague'},
         age: {'54'}
@@ -24,7 +24,7 @@ def third_challenge
         name: {'Lord Capulet'},
         age: {'50'}
         #your key/value pairs here
-        }},
+        },
       :matriarch => {
         name: {'Lady Capulet'},
         age: {'51'}
